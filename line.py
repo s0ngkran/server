718d313f4dffdd8f3d58c750b33db90dfed50754
s0ngkran <52753974+s0ngkran@users.noreply.github.com>
2020-08-14 05:50:08 +0700

    Add files via upload
    change header for send img

--- line.py
+++ line.py
@@ -1,8 +1,10 @@
 
 import requests
+import urllib.parse
+import sys
 url = 'https://notify-api.line.me/api/notify'
 token = 'kic5BCvffrnTLGoPHhiITqUYwp8DEQxZPN18ouRG6wU'
-headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
+headers = {"Authorization":"Bearer "+token}
 def send(msg):
     msg = str(msg)
     r = requests.post(url, headers=headers , data = {'message':msg})
@@ -10,6 +12,12 @@ def send(msg):
 def test():
     send('this is a test msg.')
     print('sent.')
+def send_img(img):
+    data = ({'message':'Test Image'})
+    f = {'imageFile':open(img,'rb')}
+    r = requests.post(url, headers=headers, data=data, files=f)
+    print(r.text)
+
 
 if __name__ == '__main__':
     test()

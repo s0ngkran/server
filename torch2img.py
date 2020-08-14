87e04c6d0dbed288fe9c1389341c2aee88b7be5c
s0ngkran <52753974+s0ngkran@users.noreply.github.com>
2020-08-15 03:23:55 +0700

    Update torch2img.py

--- torch2img.py
+++ torch2img.py
@@ -1,7 +1,7 @@
 import torch 
 import cv2
 import numpy as np
-def torch2img(torch):
+def write(torch):
     assert len(torch.shape) == 2
     img = np.array(torch*255)
     cv2.imwrite('temp.jpg', img)
@@ -9,7 +9,7 @@ def torch2img(torch):
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     t = torch.rand([60,60])
-    torch2img(t)
+    write(t)
     img = cv2.imread('temp.jpg')
     plt.imshow(img)
     plt.show()

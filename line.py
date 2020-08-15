7f670dd88a21b015dd4c7d8a8de343170d9c4837
s0ngkran <52753974+s0ngkran@users.noreply.github.com>
2020-08-15 10:23:27 +0700

    Add files via upload

--- line.py
+++ line.py
@@ -2,31 +2,38 @@
 import requests
 import urllib.parse
 import sys
-url = 'https://notify-api.line.me/api/notify'
-token = 'kic5BCvffrnTLGoPHhiITqUYwp8DEQxZPN18ouRG6wU'
-headers = {"Authorization":"Bearer "+token}
-def send(msg):
-    msg = str(msg)
-    r = requests.post(url, headers=headers , data = {'message':msg})
-    # print( r.text)
-def test_send():
-    send('this is a test msg.')
-    print('sent.')
+import os
+class Line():
+    def __init__(self, filename):
 
-def send_img(img):
-    data = ({'message':'Test Image'})
-    f = {'imageFile':open(img,'rb')}
-    r = requests.post(url, headers=headers, data=data, files=f)
-    print(r.text)
-def send_torch(tensor):
-    import torch2img 
-    torch2img.write(tensor)
-    send_img('temp.jpg')
-def test_send_torch():
-    import torch
-    t = torch.rand([100,100])
-    send_torch(t)
+        self.url = 'https://notify-api.line.me/api/notify'
+        self.token = 'kic5BCvffrnTLGoPHhiITqUYwp8DEQxZPN18ouRG6wU'
+        self.headers = {"Authorization":"Bearer "+self.token}
+        self.filename = filename
+
+    def send(self, msg):
+        msg = str(self.filename + '\n>' + msg)
+        r = requests.post(self.url, headers=self.headers , data = {'message':msg})
+        # print( r.text)
+    def test_send(self):
+        self.send('this is a test msg.')
+        print('sent.')
+
+    def send_img(self, img):
+        data = ({'message':'Test Image'})
+        f = {'imageFile':open(img,'rb')}
+        r = requests.post(self.url, headers=self.headers, data=data, files=f)
+        print(r.text)
+    def send_torch(self, tensor):
+        import torch2img 
+        torch2img.write(tensor)
+        self.send_img('temp.jpg')
+    def test_send_torch(self):
+        import torch
+        t = torch.rand([100,100])
+        self.send_torch(t)
 
 if __name__ == '__main__':
-    test_send()
+    line = Line('test.py')
+    line.test_send()
     

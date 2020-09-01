75e63f170318ad9e1ed816729130a1774c173311
s0ngkran <52753974+s0ngkran@users.noreply.github.com>
2020-09-02 02:51:31 +0700

    Add files via upload

--- line.py
+++ line.py
@@ -6,54 +6,63 @@ import os
 import time
 class Line():
     
-    def __init__(self, filename):
+    def __init__(self, filename, turnoff=False):
         
         self.url = 'https://notify-api.line.me/api/notify'
         self.token = 'kic5BCvffrnTLGoPHhiITqUYwp8DEQxZPN18ouRG6wU'
         self.headers = {"Authorization":"Bearer "+self.token}
         self.filename = filename
+        self.turnoff = turnoff
         # self.send('starting... ')
     def post(self, data, headers='', files=''):
-        if headers=='': headers=self.headers
-        success = False
-        while not success:
-            try:
-                r = requests.post(self.url, headers=headers, data=data, files=files)
-                success = True
-                return r
-            except:
-                print('requests not success wait 10 sec')
-                time.sleep(10)
+        if not self.turnoff:
+            if headers=='': headers=self.headers
+            success = False
+            while not success:
+                try:
+                    r = requests.post(self.url, headers=headers, data=data, files=files)
+                    success = True
+                    return r
+                except:
+                    print('requests not success wait 10 sec')
+                    time.sleep(10)
 
     def send(self, msg):
-        msg = str(self.filename + '\n>>>' + msg)
-        self.post({'message':msg})
-       
-        # print( r.text)
+        if not self.turnoff:
+            msg = str(self.filename + '\n>>>' + msg)
+            self.post({'message':msg})
+        
+            # print( r.text)
     def send_group(self, msg):
-        group_token = '38yvBf3KyRqRrOGj312UhycDcNGTPjNCOlTm9R9xuyU'
-        headers = {"Authorization":"Bearer " + group_token}
-        self.post({'message':msg}, headers=headers)
-        # print( r.text)
+        if not self.turnoff:
+            group_token = '38yvBf3KyRqRrOGj312UhycDcNGTPjNCOlTm9R9xuyU'
+            headers = {"Authorization":"Bearer " + group_token}
+            self.post({'message':msg}, headers=headers)
+            # print( r.text)
     def test_send_group(self):
-        self.send_group('im notify')
+        if not self.turnoff:
+            self.send_group('im notify')
     def test_send(self):
-        self.send('this is a test msg.')
-        print('sent.')
+        if not self.turnoff:
+            self.send('this is a test msg.')
+            print('sent.')
 
     def send_img(self, img, text='img from server'):
-        data = ({'message':text})
-        f = {'imageFile':open(img,'rb')}
-        self.post(data, files=f)
-        # print(r.text)
+        if not self.turnoff:
+            data = ({'message':text})
+            f = {'imageFile':open(img,'rb')}
+            self.post(data, files=f)
+            # print(r.text)
     def send_torch(self, tensor):
-        import torch2img 
-        torch2img.write(tensor)
-        self.send_img('temp.jpg')
+        if not self.turnoff:
+            import torch2img 
+            torch2img.write(tensor)
+            self.send_img('temp.jpg')
     def test_send_torch(self):
-        import torch
-        t = torch.rand([100,100])
-        self.send_torch(t)
+        if not self.turnoff:
+            import torch
+            t = torch.rand([100,100])
+            self.send_torch(t)
 
 if __name__ == '__main__':
     line = Line(os.path.basename(__file__))

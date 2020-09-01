8ff23b0f3dda7617b1619c0c4f2caadf835c8593
s0ngkran <52753974+s0ngkran@users.noreply.github.com>
2020-09-02 05:09:17 +0700

    Add files via upload

--- monitor2.py
+++ monitor2.py
@@ -3,120 +3,108 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 import time
-host = "202.28.93.225"
-port = 22
-username = "sk"
-password = "sk"
+from ck_loss import getfromline
 
-# command = "cd skd/batchnorm/train02; cat tr32_seed10"
-command = "cd skd/batchnorm/train02; cat "
-command_ = input('file => ')
-command = command + command_
-print('''
-\nshow_position
-1 2 3
-4 5 6
-''')
-show_pos = input('show_position => ')
-x = int(show_pos)-1 if int(show_pos)<4 else int(show_pos)-4
-x *= 500
-y = 0 if int(show_pos) < 4 else 1
-y *= 500
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(host, port, username, password)
-
-def readfile(dat):
-    loss = []
+def readfile(data):
+    training = []
     validation = []
-    testing = []
-    for i in range(len(dat)):
-        if dat[i][:6] == 'epoch=':
-            try:
-                loss_ = dat[i].split('loss=')[1][:-1]
-                ep = dat[i].split('loss=')[0].split()[1]
-                loss_ = float(loss_)
-                ep = int(ep)
-                loss.append([ep,loss_])
-            except: pass
-            try:
-                a = dat[i].split()
-                ep = a[0].split('epoch=')[1]
-                loss_ = a[1].split('loss=')[1]
-                loss_ = float(loss_)
-                ep = int(ep)
-                loss.append([ep,loss_])
-            except: pass
-            try:
-                loss_ = dat[i].split('loss_va=')[1][:-1]
-                ep = dat[i].split('loss_va=')[0].split()[1]
-                loss_ = float(loss_)
-                ep = int(ep)
-                validation.append([ep,loss_])
-            except: pass
-            try:
-                a = dat[i].split()
-                ep = a[0].split('epoch=')[1]
-                loss_ = a[1].split('loss_va=')[1]
-                loss_ = float(loss_)
-                ep = int(ep)
-                validation.append([ep,loss_])
-            except: pass
-            try:
-                loss_ = dat[i].split('te_acc=')[1][:-1]
-                ep = dat[i].split('te_acc=')[0].split()[1]
-                loss_ = float(loss_)
-                ep = int(ep)
-                testing.append([ep,loss_])
-            except: pass
-            try:
-                a = dat[i].split()
-                ep = a[0].split('epoch=')[1]
-                loss_ = a[1].split('te_acc=')[1]
-                loss_ = float(loss_)
-                ep = int(ep)
-                testing.append([ep,loss_])
-            except: pass
-    lo = np.array(loss)
-    plt.plot(lo[:,0],lo[:,1],'r.',label='training loss')
-    va = np.array(validation)
-    plt.plot(va[:,0],va[:,1],'b.-',label='validation loss')
-    te = np.array(testing)
-    plt.plot(te[:,0],te[:,1]/10000,'g.-',label='testing accuracy')
+    testing1 = []
+    testing2 = []
+    for line in data:
+        try:
+            epoch = getfromline(line, 'epoch=')
+            tr_loss = getfromline(line, 'loss=')
+            va_loss = getfromline(line, 'loss_va=')
+            te_loss = getfromline(line, 'loss_te=')
+
+            if tr_loss != None: training.append([epoch, tr_loss])
+            if va_loss != None: validation.append([epoch, va_loss])
+            if te_loss != None:
+                list_ = line.split('loss_te=')[1]
+                if list_[-1] == '\n': list_ = list_[:-1]
+                assert list_[0] == '[', 'check loss_te format'
+                # split [ , ] format
+                te_loss = list_.split('[')[1].split(']')[0].split(',')
+                te_loss = [float(i) for i in te_loss]
+                testing1.append([epoch, te_loss[0]])
+                testing2.append([epoch, te_loss[1]])
+        except :pass
+    training = np.array(training)
+    validation = np.array(validation)
+    plt.plot(training[:,0],training[:,1],'r.',label='training loss')
+    plt.plot(validation[:,0],validation[:,1],'b.-',label='validation loss')
+    if testing1 != []:
+        testing1 = np.array(testing1)
+        testing2 = np.array(testing2)
+        plt.plot(testing1[:,0],testing1[:,1]*400,'m.-',label='testing1 acc*400')
+        plt.plot(testing2[:,0],testing2[:,1]*400,'g.-',label='testing2 acc*400')
+
     plt.legend()
     plt.xlabel('epoch')
     plt.ylabel('loss')
     #plt.ylim((0,4))
     plt.grid()   
 
-fail_state = 0
-while True:
-    try:
-        stdin, stdout, stderr = ssh.exec_command(command)
-        lines = stdout.readlines()
-        readfile(lines)
-        plt.title(command.split('cat ')[1] +'\n'+ str(datetime.datetime.now()))
-        mngr = plt.get_current_fig_manager()
-        # to put it into the upper left corner for example:
-        mngr.window.setGeometry(x,y+30,500, 500)
-        
-        if fail_state == 1:
-            fail_state = 0
-            print('success...')
+def run(command, show_pos):
+    host = "202.28.93.225"
+    port = 22
+    username = "sk"
+    password = "sk"
 
-        with open('monitor.txt','r') as f:
-            realtime = f.readline()
-        if realtime == 'realtime':
-            print('realtime mode '+command.split('cat ')[1])
-            plt.pause(120)
-        else:
-            plt.show()
-        
-        plt.cla()
+    # command = "cd skd/batchnorm/train02; cat tr32_seed10"
+    # command = "cd skd/train0027; cat tr01.txt"
+    # command_ = input('file => ')
+    # command = command + command_
+    # print('''
+    # \nshow_position
+    # 1 2 3
+    # 4 5 6
+    # ''')
+    # show_pos = input('show_position => ')
+    print('show_pos=',show_pos)
+    x = int(show_pos)-1 if int(show_pos)<4 else int(show_pos)-4
+    x *= 500
+    y = 0 if int(show_pos) < 4 else 1
+    y *= 500
 
-    except :
-        print('try again in 10sec...')
-        fail_state = 1
-        time.sleep(10)
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, port, username, password)
+    fail_state = 0
+    while True:
+        try:
+            stdin, stdout, stderr = ssh.exec_command(command)
+            lines = stdout.readlines()
+            readfile(lines)
+            plt.title(command.split('cat ')[1] +'\n'+ str(datetime.datetime.now()))
+            mngr = plt.get_current_fig_manager()
+            # to put it into the upper left corner for example:
+            mngr.window.setGeometry(x,y+30,500, 500)
+            
+            if fail_state == 1:
+                fail_state = 0
+                print('success...')
+
+            with open('monitor.txt','r') as f:
+                realtime = f.readline()
+            if realtime == 'realtime':
+                print('realtime mode '+command.split('cat ')[1])
+                plt.pause(120)
+            else:
+                plt.show()
+            
+            plt.cla()
+
+        except :
+            print(lines, 'try again in 10sec...')
+            fail_state = 1
+            time.sleep(10)
+
+            
+if __name__ == "__main__":
+
+    command = "cd skd/train0027; cat tr01.txt"
+    show_pos = 1
+    run(command, show_pos)
     

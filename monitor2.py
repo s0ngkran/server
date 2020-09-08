fe08f22d8e97ffaf063520c8950f0b227eef03c4
s0ngkran <52753974+s0ngkran@users.noreply.github.com>
2020-09-08 21:12:45 +0700

    Add files via upload

--- monitor2.py
+++ monitor2.py
@@ -5,8 +5,8 @@ import datetime
 import time
 from ck_loss import getfromline
 
-
-def readfile(data):
+def readfile(data, lim=''):
+    if lim == '': lim = None
     training = []
     validation = []
     testing1 = []
@@ -37,20 +37,22 @@ def readfile(data):
     if testing1 != []:
         testing1 = np.array(testing1)
         testing2 = np.array(testing2)
-        plt.plot(testing1[:,0],testing1[:,1]*400,'m.-',label='testing1 acc*400')
-        plt.plot(testing2[:,0],testing2[:,1]*400,'g.-',label='testing2 acc*400')
+        plt.plot(testing1[:,0],testing1[:,1]*100,'m.-',label='testing1 acc*100')
+        plt.plot(testing2[:,0],testing2[:,1]*100,'g.-',label='testing2 acc*100')
 
     plt.legend()
     plt.xlabel('epoch')
     plt.ylabel('loss')
-    #plt.ylim((0,4))
-    plt.grid()   
+    if lim != None:
+        plt.ylim((0,lim))
+      
+    return testing1, testing2
 
-def run(command, show_pos):
-    host = ###
-    port = ###
-    username = ###
-    password = ###
+def run(command, show_pos=1, lim='', check_test_mode=False, epoch='',tr=None):
+    host = "202.28.93.225"
+    port = 22
+    username = "sk"
+    password = "sk"
 
     # command = "cd skd/batchnorm/train02; cat tr32_seed10"
     # command = "cd skd/train0027; cat tr01.txt"
@@ -62,7 +64,7 @@ def run(command, show_pos):
     # 4 5 6
     # ''')
     # show_pos = input('show_position => ')
-    print('show_pos=',show_pos)
+    # print('show_pos=',show_pos)
     x = int(show_pos)-1 if int(show_pos)<4 else int(show_pos)-4
     x *= 500
     y = 0 if int(show_pos) < 4 else 1
@@ -76,7 +78,7 @@ def run(command, show_pos):
         try:
             stdin, stdout, stderr = ssh.exec_command(command)
             lines = stdout.readlines()
-            readfile(lines)
+            testing1, testing2 = readfile(lines, lim=lim)
             plt.title(command.split('cat ')[1] +'\n'+ str(datetime.datetime.now()))
             mngr = plt.get_current_fig_manager()
             # to put it into the upper left corner for example:
@@ -86,25 +88,72 @@ def run(command, show_pos):
                 fail_state = 0
                 print('success...')
 
-            with open('monitor.txt','r') as f:
-                realtime = f.readline()
-            if realtime == 'realtime':
-                print('realtime mode '+command.split('cat ')[1])
-                plt.pause(120)
+            if check_test_mode:
+                print('file:',tr,'epoch', epoch)
+                for ep, val1 in testing1:
+                    if ep == epoch:
+                        print('AP testing1=', val1)
+                        break
+                for ep, val2 in testing2:
+                    if ep == epoch:
+                        print('AP testing2=', val2)
+                        break
+                print('mAP =',(val1+val2)/2)
+                break
+                
+                print('AP testing2=',testing2[50])
             else:
-                plt.show()
-            
-            plt.cla()
+                with open('monitor.txt','r') as f:
+                    realtime = f.readline()
+
+                if realtime == 'realtime':
+                    print('realtime mode '+command.split('cat ')[1])
+                    plt.pause(120)
+                else:
+                    plt.grid() 
+                    plt.show()
+                    break
+                
+                plt.cla()
 
         except :
-            print(lines, 'try again in 10sec...')
+            print('\n\ntry again in 10sec...')
             fail_state = 1
             time.sleep(10)
 
-            
-if __name__ == "__main__":
-
-    command = "cd skd/train0027; cat tr01.txt"
+def get_input(oldname):
+    epoch = 0
+    cmode = False
     show_pos = 1
-    run(command, show_pos)
+    tr = input('name>>')
+    if tr == '':
+        tr = oldname
+    elif 'tr' not in tr:
+        epoch = int(tr)
+        tr = oldname
+        cmode = True
+        
+    elif ' ' in tr:
+        cmode = True
+        epoch = tr.split(' ')[1]
+        epoch = int(epoch)
+        tr = tr.split(' ')[0]
+    else:
+        print('''
+        \nshow_position
+        1 2 3
+        4 5 6
+        ''')
+        show_pos = input('show_position => ')
+        if show_pos == '':show_pos = 1
+    command = "cd skd/train0027; cat %s.txt"%tr
+    run(command, show_pos=show_pos, lim=100, check_test_mode=cmode, epoch=epoch, tr=tr)
+    return tr
+if __name__ == "__main__":
+    tr = None
+    while True:
+        tr = get_input(tr)
+        print('')
+    print('end')
+    
     
